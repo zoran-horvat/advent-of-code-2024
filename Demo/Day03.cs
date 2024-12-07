1c69a29dc412a31ebe6691e59610ee3487ac06d5
Zoran Horvat <zh@codinghelmet.com>
2024-12-07 02:51:55 +0100

    Improved Day 3 to use LINQ Aggregate to evaluate any sequence of instructions

--- Demo/Day03.cs
+++ Demo/Day03.cs
@@ -6,8 +6,8 @@ static class Day03
     {
         var instructions = Console.In.ReadLines().SelectMany(Parse).ToList();
 
-        int sum = instructions.OfType<Multiply>().Sum(instruction => instruction.Product);
-        int sumWithExclusions = instructions.WithExclusions().Sum(instruction => instruction.Product);
+        int sum = instructions.OfType<Multiply>().Evaluate();
+        int sumWithExclusions = instructions.Evaluate();
 
         Console.WriteLine($"Sum: {sum}");
         Console.WriteLine($"Sum with exclusions: {sumWithExclusions}");
@@ -20,16 +20,15 @@ static class Day03
                 : match.Groups["do"].Success && match.Groups["do"].Value == "don't" ? new Stop()
                 : new Multiply(int.Parse(match.Groups["a"].Value), int.Parse(match.Groups["b"].Value)));
 
-    private static IEnumerable<Multiply> WithExclusions(this IEnumerable<Instruction> instructions)
-    {
-        bool include = true;
-        foreach (Instruction instruction in instructions)
+    private static int Evaluate(this IEnumerable<Instruction> instructions) => instructions.Aggregate(
+        (sum: 0, include: true),
+        (acc, instruction) => instruction switch
         {
-            if (instruction is Continue) include = true;
-            else if (instruction is Stop) include = false;
-            else if (instruction is Multiply multiply && include) yield return multiply;
-        }
-    }
+            Continue => (acc.sum, true),
+            Stop => (acc.sum, false),
+            Multiply multiply when acc.include => (acc.sum + multiply.Product, acc.include),
+            _ => acc
+        }).sum;
 }
 
 abstract record Instruction;
