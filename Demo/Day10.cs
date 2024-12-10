bf4120d96b3201417f80385b9015f53a24601c4a
Zoran Horvat <zh@codinghelmet.com>
2024-12-10 12:35:08 +0100

    Added Day 10 solution

---
+++ Demo/Day10.cs
@@ -0,0 +1,66 @@
+static class Day10
+{
+    public static void Run()
+    {
+        char[][] map = Console.In.ReadMap();
+        var startingPoints = map.GetStartingPoints().ToList();
+
+        int totalScore = startingPoints.Sum(start => map.Walk(start).CountHikingTrails(map));
+        int distinctTrailScore = startingPoints.Sum(start => map.Walk(start).CountDistinctHikingTrails(map));
+
+        Console.WriteLine($"   Total hiking trails: {totalScore}");
+        Console.WriteLine($"Distinct hiking trails: {distinctTrailScore}");
+    }
+
+    private static int CountDistinctHikingTrails(this IEnumerable<((int row, int col) point, int count)> visitedPoints, char[][] map) =>
+        visitedPoints.Where(node => map.At(node.point) == '9').Sum(node => node.count);
+
+    private static int CountHikingTrails(this IEnumerable<((int row, int col) point, int count)> visitedPoints, char[][] map) =>
+        visitedPoints.Count(node => map.At(node.point) == '9');
+
+    private static IEnumerable<((int row, int col) point, int count)> Walk(this char[][] map, (int row, int col) start)
+    {
+        var pointToCount = new Dictionary<(int row, int col), int>();
+        var queue = new Queue<((int row, int col) point, int count)>();
+
+        queue.Enqueue((start, 1));
+
+        while (queue.Count > 0)
+        {
+            var (point, count) = queue.Dequeue();
+
+            if (!pointToCount.TryGetValue(point, out int priorCount)) priorCount = 0;
+            pointToCount[point] = priorCount + count;
+
+            foreach (var neighbor in map.GetUphillNeighbors(point)) queue.Enqueue((neighbor, count));
+        }
+
+        return pointToCount.Select(keyValue => (keyValue.Key, keyValue.Value));
+    }
+
+    private static IEnumerable<(int row, int col)> GetUphillNeighbors(this char[][] map, (int row, int col) point) =>
+        new[]
+        {
+            (point.row - 1, point.col), (point.row + 1, point.col),
+            (point.row, point.col - 1), (point.row, point.col + 1) 
+        }
+        .Where(neighbor => map.IsUphill(point, neighbor));
+
+    private static bool IsUphill(this char[][] map, (int row, int col) a, (int row, int col) b) =>
+        map.IsOnMap(b) && map.At(a) < '9' && map.At(b) == map.At(a) + 1;
+        
+    private static bool IsOnMap(this char[][] map, (int row, int col) point) =>
+        point.row >= 0 && point.row < map.Length && point.col >= 0 && point.col < map[point.row].Length;
+
+    private static char At(this char[][] map, (int row, int col) point) =>
+        map[point.row][point.col];
+
+    private static IEnumerable<(int row, int col)> GetStartingPoints(this char[][] map) =>
+        from row in Enumerable.Range(0, map.Length)
+        from col in Enumerable.Range(0, map[row].Length)
+        where map[row][col] == '0'
+        select (row, col);
+
+    private static char[][] ReadMap(this TextReader text) =>
+        text.ReadLines().Select(line => line.ToCharArray()).ToArray();
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,7 +1,7 @@
 ﻿Action[] problemSolutions =
 [
     Day01.Run, Day02.Run, Day03.Run, Day04.Run, Day05.Run,
-    Day06.Run, Day07.Run, Day08.Run, Day09.Run
+    Day06.Run, Day07.Run, Day08.Run, Day09.Run, Day10.Run
 ];
 
 foreach (int index in ProblemIndices())
