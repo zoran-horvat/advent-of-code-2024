ec50446f2ceaa0fdb76aed2c71df0784ce93eeee
Zoran Horvat <zh@codinghelmet.com>
2025-06-04 14:56:28 +0200

    Day 15 Improved the implementation

--- Demo/Day15.cs
+++ Demo/Day15.cs
@@ -4,212 +4,130 @@ static class Day15
 {
     public static void Run()
     {
-        var state = Console.In.ReadMap().ToState();
-        var instructions = Console.In.ReadInstructions().ToList();
+        var state = Console.In.ReadState();
+        var steps = Console.In.ReadSteps().ToList();
 
-        Stopwatch sw = Stopwatch.StartNew();
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        var totalGps = state.Apply(steps).Boxes.Index.Values.Sum(GetGps);
+        var scaledGps = state.Scale().Apply(steps).Boxes.Index.Values.Sum(GetGps) / 2;
+        stopwatch.Stop();
 
-        var finalState = state.Simulate(instructions);
-        var finalStateScaled = state.ScaledUp().Simulate(instructions);
-
-        int totalGps = finalState.ToGps();
-        int scaledGps = finalStateScaled.ToGps();
-        sw.Stop();
-
-        Console.WriteLine($"       Total GPS: {totalGps}");
-        Console.WriteLine($"Total scaled GPS: {scaledGps}");
-        Console.WriteLine($"    Completed in: {sw.ElapsedMilliseconds} ms");
-
-        Console.Out.Print(finalState.ToMap());
-        Console.Out.Print(finalStateScaled.ToMap());
+        Console.WriteLine($" Total GPS: {totalGps}");
+        Console.WriteLine($"Scaled GPS: {scaledGps}");
+        Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
     }
 
-    private static void Print(this TextWriter writer, char[][] map) =>
-        map.Select(row => new string(row)).ToList().ForEach(writer.WriteLine);
+    private static State Scale(this State state) =>
+        new State(
+            state.Robot.ScaleRobot(),
+            state.Boxes.Index.Values.Select(box => box.Position.ScaleBox()).ToBoxes(),
+            state.Walls.SelectMany(ScaleWall).ToHashSet());
+    
+    private static Point ScaleRobot(this Point robot) =>
+        new(robot.Row, robot.Column * 2);
 
-    private static char[][] ToMap(this State state)
+    private static Box ScaleBox(this Point box) =>
+        new(new(box.Row, box.Column * 2), 2);
+    
+    private static IEnumerable<Point> ScaleWall(this Point wall) =>
+        [ wall with { Column = wall.Column * 2 }, wall with { Column = wall.Column * 2 + 1 } ];
+
+    private static int GetGps(this Box box) =>
+        box.Position.Row * 100 + box.Position.Column;
+
+    private static State Apply(this State state, IEnumerable<Step> steps) =>
+        steps.Aggregate(state, Apply);
+
+    private static State Apply(this State state, Step step) =>
+        state.GetMove(step).ApplyTo(state);
+
+    private static State ApplyTo(this (Step step, IEnumerable<Box> boxes) move, State state) =>
+        new State(
+            state.Robot.Apply(move.step),
+            state.Boxes.Index.Values.Except(move.boxes).Concat(move.boxes.Apply(move.step)).ToBoxes(),
+            state.Walls);
+
+    private static IEnumerable<Box> Apply(this IEnumerable<Box> boxes, Step step) =>
+        boxes.Select(box => box with { Position = box.Position.Apply(step) });
+    
+    private static Point Apply(this Point point, Step step) =>
+        new(point.Row + step.RowDiff, point.Column + step.ColumnDiff);
+
+    private static (Step step, IEnumerable<Box> boxes) GetMove(this State state, Step step)
     {
-        int width = state.Walls.Max(point => point.Col) + 1;
-        int height = state.Walls.Max(point => point.Row) + 1;
+        var boxesToMove = new HashSet<Box>();
 
-        char[][] map = Enumerable.Range(0, height).Select(_ => Enumerable.Repeat('.', width).ToArray()).ToArray();
-
-        state.Walls.ForEach(point => map[point.Row][point.Col] = '#');
-
-        void drawBox(Box box)
+        var pushing = new List<Point>() { state.Robot.Apply(step) };
+        while (pushing.Count > 0)
         {
-            if (box.Points.Length == 1)
-            {
-                map[box.Points[0].Row][box.Points[0].Col] = 'O';
-            }
-            else
-            {
-                map[box.Points[0].Row][box.Points[0].Col] = '[';
-                map[box.Points[1].Row][box.Points[1].Col] = ']';
-            }
+            if (pushing.Any(state.Walls.Contains)) return (new Step(0, 0), Enumerable.Empty<Box>());
+            pushing = state.Boxes.GetBoxes(pushing)
+                .Where(boxesToMove.Add)
+                .SelectMany(ToPositions)
+                .Select(pair => pair.position.Apply(step))
+                .ToList();
         }
 
-        map[state.Robot.Row][state.Robot.Col] = '@';
-
-        state.Boxes.ForEach(drawBox);
-
-        return map;
+        return (step, boxesToMove);
     }
 
-    private static State Simulate(this State state, IEnumerable<Direction> instructions) =>
-        instructions.Aggregate(state.Copy(), (current, direction) => current.Move(direction));
+    private static State ReadState(this TextReader reader) =>
+        reader.ReadMap().ToState();
 
-    private static State Move(this State state, Direction direction)
-    {
-        var pressingAt = state.Robot.Move(direction);
-        if (state.Walls.Contains(pressingAt)) return state;
+    private static State ToState(this char[][] map) =>
+        new(map.FindRobot(), map.FindBoxes().ToBoxes(), map.FindWalls().ToHashSet());
 
-        var boxesToPush = state.GetPushingGroup(direction).ToList();
-        if (!boxesToPush.CanMoveAll(direction, state)) return state;
+    private static Point FindRobot(this char[][] map) =>
+        map.PositionsOf('@').Single();
 
-        var boxesToStay = state.Boxes.ToHashSet().Except(boxesToPush);
-
-        return state with
-        {
-            Boxes =  boxesToPush.MoveAll(direction).Concat(boxesToStay).ToArray(),
-            Robot = pressingAt
-        };
-
-        // Same thing, mutable:
-        // var pressingAt = state.Robot.Move(direction);
-        // if (state.Walls.Contains(pressingAt)) return state;
-
-        // var boxesToPush = state.GetPushingGroup(direction).ToHashSet();
-        // if (!boxesToPush.CanMoveAll(direction, state)) return state;
-
-        // for (int i = 0; i < state.Boxes.Length; i++)
-        // {
-        //     if (!boxesToPush.Contains(state.Boxes[i])) continue;
-        //     state.Boxes[i] = state.Boxes[i].Move(direction);
-        // }
-
-        // return state with { Robot = pressingAt };
-    }
-
-    private static IEnumerable<Point> GetRobots(this char[][] map) =>
+    private static IEnumerable<Box> FindBoxes(this char[][] map) =>
+        map.PositionsOf('O').Select(pos => new Box(pos, 1));
+    
+    private static IEnumerable<Point> FindWalls(this char[][] map) =>
+        map.PositionsOf('#');
+    
+    private static IEnumerable<Point> PositionsOf(this char[][] map, char c) =>
         from row in Enumerable.Range(0, map.Length)
         from col in Enumerable.Range(0, map[row].Length)
-        where map[row][col] == '@'
+        where map[row][col] == c
         select new Point(row, col);
 
-    private static IEnumerable<Box> MoveAll(this IEnumerable<Box> boxes, Direction direction) =>
-        boxes.Select(box => box.Move(direction));
+    private static IEnumerable<Step> ReadSteps(this TextReader reader) =>
+        reader.ReadLines().SelectMany(line => line.ToCharArray()).Select(ToStep);
 
-    private static bool CanMoveAll(this IEnumerable<Box> boxes, Direction direction, State state) =>
-        boxes.All(box => box.CanMove(direction, state));
-
-    private static IEnumerable<Box> GetPushingGroup(this State state, Direction direction)
-    {
-        HashSet<Box> notPushingBoxes = state.Boxes.ToHashSet();
-        Dictionary<Point, Box> pointToBox = state.Boxes.SelectMany(GetPoints).ToDictionary();
-
-        Queue<Point> pressurePoints = new([state.Robot.Move(direction)]);
-
-        while (pressurePoints.Count > 0)
-        {
-            Point current = pressurePoints.Dequeue();
-            if (!pointToBox.TryGetValue(current, out Box? pushingBox)) continue;
-
-            if (notPushingBoxes.Contains(pushingBox))
-            {
-                notPushingBoxes.Remove(pushingBox);
-                yield return pushingBox;
-            }
-
-            pushingBox.Points
-                .Select(point => point.Move(direction))
-                .Where(point => !pushingBox.Points.Contains(point))
-                .ForEach(pressurePoints.Enqueue);
-        }
-    }
-
-    private static IEnumerable<(Point point, Box box)> GetPoints(this Box box) =>
-        box.Points.Select(point => (point, box));
-
-    private static HashSet<Box> ToHashSet(this IEnumerable<Box> boxes) =>
-        new HashSet<Box>(boxes,
-            EqualityComparer<Box>.Create(
-                (a, b) => a?.Points[0] == b?.Points[0],
-                box => box?.Points[0].GetHashCode() ?? 0));
-
-    private static Box Move(this Box box, Direction direction) =>
-        new(box.Points.Select(point => point.Move(direction)).ToArray());
-
-    private static bool CanMove(this Box box, Direction direction, State state) =>
-        box.Points.All(point => !state.Walls.Contains(point.Move(direction)));
-
-    private static Point Move(this Point point, Direction direction) =>
-        new(point.Row + direction.RowStep, point.Col + direction.ColStep);
-
-    private static int ToGps(this State state) => state.Boxes.Sum(ToGps);
-
-    private static int ToGps(this Box box) => box.Points[0].ToGps();
-
-    private static int ToGps(this Point point) => point.Row * 100 + point.Col;
-
-    private static Direction ToDirection(this char direction) => direction switch
+    private static Step ToStep(this char arrow) => arrow switch
     {
         '>' => new(0, 1),
         '<' => new(0, -1),
         '^' => new(-1, 0),
-        'v' => new(1, 0),
-        _ => throw new ArgumentException($"Invalid direction: {direction}")
+        _ => new(1, 0),
     };
 
-    private static State ScaledUp(this State state) =>
-        new(state.Boxes.Select(ScaledUp).ToArray(),
-            state.Robot.ScaledUpRobot(),
-            state.Walls.SelectMany(ScaledUpWall).ToHashSet());
-
-    private static Box ScaledUp(this Box box) => box.Points switch
-    {
-        [ (int row, int col) ] => new Box([ new(row, 2 * col), new(row, 2 * col + 1) ]),
-        _ => throw new ArgumentException("The box is already scaled")
-    };
-
-    private static Point ScaledUpRobot(this Point point) =>
-        new(point.Row, 2 * point.Col);
-
-    private static Point[] ScaledUpWall(this Point wall) =>
-        [ new(wall.Row, 2 * wall.Col), new(wall.Row, 2 * wall.Col + 1) ];
-
-    private static State Copy(this State state) =>
-        state with { Boxes = state.Boxes.ToArray() };
-
-    private static State ToState(this char[][] map) =>
-        new(map.GetBoxes().ToArray(), map.GetRobots().First(), map.GetWalls().ToHashSet());
-
-    private static IEnumerable<Point> GetWalls(this char[][] map) =>
-        from row in Enumerable.Range(0, map.Length)
-        from col in Enumerable.Range(0, map[row].Length)
-        where map[row][col] == '#'
-        select new Point(row, col);
-
-    private static IEnumerable<Box> GetBoxes(this char[][] map) =>
-        from row in Enumerable.Range(0, map.Length)
-        from col in Enumerable.Range(0, map[row].Length)
-        where map[row][col] == 'O'
-        select new Box([ new Point(row, col) ]);
-
-    private static char[][] ReadMap(this TextReader text) =>
-        text.ReadLines()
+    private static char[][] ReadMap(this TextReader reader) =>
+        reader.ReadLines()
             .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
-            .Select(row => row.ToCharArray())
+            .Select(line => line.ToCharArray())
             .ToArray();
+    
+    private static IEnumerable<Box> GetBoxes(this Boxes boxes, IEnumerable<Point> positions) =>
+        positions.Where(boxes.Index.ContainsKey)
+            .Select(pos => boxes.Index[pos])
+            .Distinct();
 
-    private static IEnumerable<Direction> ReadInstructions(this TextReader text) =>
-        text.ReadLines().SelectMany(line => line.Select(c => c.ToDirection()));
+    private static Boxes ToBoxes(this IEnumerable<Box> boxes) =>
+        new Boxes(boxes.SelectMany(ToPositions).ToDictionary(pair => pair.position, pair => pair.box));
 
-    record Box(Point[] Points);
+    private static IEnumerable<(Point position, Box box)> ToPositions(this Box box) =>
+        Enumerable.Range(0, box.Size)
+            .Select(i => (new Point(box.Position.Row, box.Position.Column + i), box));
+        
+    record State(Point Robot, Boxes Boxes, HashSet<Point> Walls);
 
-    record State(Box[] Boxes, Point Robot, HashSet<Point> Walls);
+    record Boxes(Dictionary<Point, Box> Index);
 
-    record Point(int Row, int Col);
+    record struct Box(Point Position, int Size);
 
-    record Direction(int RowStep, int ColStep);
+    record struct Step(int RowDiff, int ColumnDiff);
+
+    record struct Point(int Row, int Column);
 }
